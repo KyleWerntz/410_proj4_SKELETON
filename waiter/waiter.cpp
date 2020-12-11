5e06b307ffa69de8d58ab30a97377c4ad64c848d
Kyle Werntz <kyle.werntz.18@cnu.edu>
2020-12-10 23:31:38 -0500

    waiter finished (maybe not correct)

--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -2,6 +2,8 @@
 #include "stdlib.h"
 
 #include "../includes/waiter.h"
+#include "../includes/file_IO.h"
+#include "../includes/externs.h"
 
 using namespace std;
 
@@ -14,9 +16,18 @@ Waiter::~Waiter()
 
 //gets next Order(s) from file_IO
 int Waiter::getNext(ORDER &anOrder){
-	return SUCCESS;
+	return myIO.getNext(anOrder);
 }
 
 void Waiter::beWaiter() {
+	int i = SUCCESS;
+	while (i == SUCCESS) {
+		ORDER anOrder;
+		i = getNext(anOrder);
+		order_in_Q.push(anOrder);
+		cv_order_inQ.notify_all();
+	}
+	b_WaiterIsFinished = true;
+	cv_order_inQ.notify_all();
 }
 
